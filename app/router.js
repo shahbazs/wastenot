b13d4bfd25f22b4b653e23ed7e906d184824c1e9
Benjamin Rosas <ballPointPenguin@gmx.us>
2016-08-06 10:17:32 -0500

    update the boilerplate stuff to ember-cli 2.7.0 conventions

--- app/router.js
+++ app/router.js
@@ -2,17 +2,18 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
-  this.route('home');
   this.route('about');
+  this.route('details');
   this.route('form');
+  this.route('home');
   this.route('services');
   this.route('take');
   this.route('thanks');
-  this.route('details');
 });
 
 export default Router;
