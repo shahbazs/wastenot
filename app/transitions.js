280842cf0edb5d082e33d0fa42927521b24a2328
Benjamin Rosas <ballPointPenguin@gmx.us>
2016-08-20 15:03:48 +0200

    smoother transition animations

--- app/transitions.js
+++ app/transitions.js
@@ -1,17 +1,25 @@
 export default function() {
 
-  // Home <--> Register
-  this.transition(
-    this.fromRoute('home'),
-    this.toRoute('register'),
-    this.use('toRight'),
-    this.reverse('toLeft')
-  );
-
   // Home <--> About
   this.transition(
     this.fromRoute('home'),
     this.toRoute('about'),
+    this.use('toRight'),
+    this.reverse('toLeft')
+  );
+
+  // Home <--> Services
+  this.transition(
+    this.fromRoute('home'),
+    this.toRoute('services'),
+    this.use('toRight'),
+    this.reverse('toLeft')
+  );
+
+  // Home <--> Register
+  this.transition(
+    this.fromRoute('home'),
+    this.toRoute('register'),
     this.use('toRight'),
     this.reverse('toLeft')
   );
@@ -28,6 +36,14 @@ export default function() {
   this.transition(
     this.fromRoute('register'),
     this.toRoute('details'),
+    this.use('toRight'),
+    this.reverse('toLeft')
+  );
+
+  // Details <--> Thanks
+  this.transition(
+    this.fromRoute('details'),
+    this.toRoute('thanks'),
     this.use('toRight'),
     this.reverse('toLeft')
   );
