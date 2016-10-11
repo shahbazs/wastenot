2f124039c5852d8055b239a25c3de4671743b3fd
Benjamin Rosas <ballPointPenguin@gmx.us>
2016-10-10 21:56:56 -0500

    Logo in the navbar

--- app/components/wn-navbar.js
+++ app/components/wn-navbar.js
@@ -1,9 +1,21 @@
 import Ember from 'ember';
+import EmberMaterialNavbar from 'ember-cli-materialize/components/md-navbar';
 
-const { Component, computed, inject } = Ember;
+const { computed, inject } = Ember;
 
-export default Component.extend({
+export default EmberMaterialNavbar.extend({
   session: inject.service(),
 
-  isAdmin: computed.notEmpty('session.currentAdmin')
+  isAdmin: computed.notEmpty('session.currentAdmin'),
+
+  navLinks: [
+    { text: 'About', route: 'about' },
+    { text: 'Services', route: 'services' },
+    { text: 'Register', route: 'register' }
+  ],
+
+  adminLinks: [
+    { text: 'Users', route: 'admin.users' },
+    { text: 'Logout', route: 'admin.logout' }
+  ]
 });
